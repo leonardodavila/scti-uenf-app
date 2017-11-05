5d5769e89e19e1bfe0892fd4a7a5d7be18e16c93
Leonardo Maia <leonardo.maia.davila@gmail.com>
2017-11-05 21:07:21 -0200

    card

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,49 @@ class App extends Component {
               <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
             </div>
           </div>
+          <div className="card">
+            <div className="card-keynoteInfo">
+              <figure className="card-speakerThumb">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+              </figure>
+              <div className="card-keynoteTitle">
+                <p className="card-title">John Smith</p>
+                <p className="card-speaker">@johnsmith</p>
+              </div>
+            </div>
+
+            <div className="card-keynotePreview">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-keynoteInfo">
+              <figure className="card-speakerThumb">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+              </figure>
+              <div className="card-keynoteTitle">
+                <p className="card-title">John Smith</p>
+                <p className="card-speaker">@johnsmith</p>
+              </div>
+            </div>
+
+            <div className="card-keynotePreview">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
          </div>
          <footer className="footer">
           <div className="footer-item">
