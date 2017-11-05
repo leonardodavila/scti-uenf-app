527e36a44660810226fb6d1a26729141cc5ef322
Leonardo Maia <leonardo.maia.davila@gmail.com>
2017-11-05 20:49:12 -0200

    card

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <nav className="nav">
           <img src="/images/logo.png" alt="SCTI UENF" className="nav-logo"/>
-          <input type="checkbox" defaultChecked={true} id="nav-menu_toggle" />
+          <input type="checkbox" id="nav-menu_toggle" />
           <label for="nav-menu_toggle" className="nav-menuButton">
           </label>
           <div class="nav-menu">
@@ -20,16 +20,31 @@ class App extends Component {
             <h1>7ª Semana de Ciência da Computação e Tecnologia da Informação.</h1>
             <h3> De 6 a 10 de novembro no centro de convenções da UENF.</h3>
             <h2>Como ser um bom profissional na área de TI?</h2>
-            <button> Inscreva-se </button>
+            <a className="header-button"> Inscreva-se </a>
           </div>
           <figure className="header-banner">
           </figure>
         </header>
          <div className="topics">
-          <div className="topic">ONE</div>
-          <div className="topic">TWO</div>
-          <div className="topic">THREE</div>
-          <div className="topic">FOUR</div>
+          <div className="card">
+            <div className="card-keynoteInfo">
+              <figure className="card-speakerThumb">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+              </figure>
+              <div className="card-keynoteTitle">
+                <p className="card-title">John Smith</p>
+                <p className="card-speaker">@johnsmith</p>
+              </div>
+            </div>
+
+            <div className="card-keynotePreview">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
          </div>
          <footer className="footer">
           <div className="footer-item">
