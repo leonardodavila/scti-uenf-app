6697ec2d856877e17b860afba13679cb5bfc046f
Leonardo Maia <leonardo.maia.davila@gmail.com>
2017-11-06 01:46:37 -0200

    componentização

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="nav">
-          <img src="/images/logo.png" alt="SCTI UENF" className="nav-logo"/>
-          <input type="checkbox" id="nav-menu_toggle" />
-          <label for="nav-menu_toggle" className="nav-menuButton">
-          </label>
-          <div className="nav-menu">
-            <a href="/html/" className="nav-link">HTML</a>
-            <a href="/css/" className="nav-link">CSS</a>
-          </div>
-        </nav>
-        <header className="header">
-          <div className="header-title">
-            <h1>7ª Semana de Ciência da Computação e Tecnologia da Informação.</h1>
-            <h3> De 6 a 10 de novembro no centro de convenções da UENF.</h3>
-            <h2>Como ser um bom profissional na área de TI?</h2>
-            <a className="header-button"> Inscreva-se </a>
-          </div>
-          <figure className="header-banner">
-          </figure>
-        </header>
+        <Nav />
+        <Header />
          <div className="topics">
           <div className="card">
             <div className="card-keynoteInfo">
@@ -32,17 +17,15 @@ class App extends Component {
                 <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
               </figure>
               <div className="card-keynoteTitle">
-                <p className="card-title">John Smith</p>
-                <p className="card-speaker">@johnsmith</p>
+                <p className="card-title">Wordpress</p>
+                <p className="card-speaker">@SOUL CODE</p>
               </div>
             </div>
 
             <div className="card-keynotePreview">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
+              Wordpress é um sistema de gerenciamento de conteudos (CMS) para WEB, escrito em PHP com banco de dados MySQL. Atualmente está presente em 25% de todos os sites na internet. Ao término desde curso, o participante será capaz de desenvolver sites e blogs com conteúdos totalmente dinâmicos, trabalhar e prosseguir com os estudos dessa maravilhosa tecnologia.
               <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              <time dateTime="2016-1-1">08:00 AM - 12:00 PM / 6 Nov 2017</time>
             </div>
           </div>
           <div className="card">
@@ -51,36 +34,14 @@ class App extends Component {
                 <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
               </figure>
               <div className="card-keynoteTitle">
-                <p className="card-title">John Smith</p>
-                <p className="card-speaker">@johnsmith</p>
+                <p className="card-title">Desenvolvimento de aplicações web e PWAs</p>
+                <p className="card-speaker">@Leonardo Maia</p>
               </div>
             </div>
 
             <div className="card-keynotePreview">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-keynoteInfo">
-              <figure className="card-speakerThumb">
-                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-              </figure>
-              <div className="card-keynoteTitle">
-                <p className="card-title">John Smith</p>
-                <p className="card-speaker">@johnsmith</p>
-              </div>
-            </div>
-
-            <div className="card-keynotePreview">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br />
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              Neste minicurso estudaremos alguns conceitos fundamentais e técnicas para desenvolvermos aplicações web em geral, e também estudaremos no que consistem as Progressive Web Apps. Falaremos de tecnologias como HTML5, CSS3, ECMAScript6, Frameworks Javascript, Service Workers, IndexedDB, etc.. E estudaremos juntos o desenvolvimento de uma Progressive Web App.
+              <time dateTime="2016-1-1">08:00 AM - 12:00 PM - 6 Nov 2017</time>
             </div>
           </div>
           <div className="card"></div>
@@ -89,16 +50,7 @@ class App extends Component {
           <div className="card"></div>
           <div className="card"></div>
          </div>
-         <footer className="footer">
-          <div className="footer-item">
-             <h4> Sobre o evento </h4>
-             <p> A Semana de Ciência da Computação e Tecnologia da Informação (SCTI) é um dos eventos que compõe a Semana Acadêmica Unificada da UENF que todo ano reúne os alunos de graduação para participar de palestras, feiras, apresentações culturais, minicursos entre diversas outras atividades.</p>
-          </div>
-          <div className="footer-item">
-            <h4> Endereço </h4>
-            <p> Universidade Estadual do Norte Fluminense Darcy Ribeiro, Av. Alberto Lamego, 2000 - Parque Califórnia, Campos dos Goytacazes - RJ, CEP: 28013-602</p>
-          </div>
-        </footer>
+         <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+	<footer class="footer">
+		<div class="footer-item">
+			<h4> Sobre o evento </h4>
+			<p> A Semana de Ciência da Computação e Tecnologia da Informação (SCTI) é um dos eventos que compõe a Semana Acadêmica Unificada da UENF que todo ano reúne os alunos de graduação para participar de palestras, feiras, apresentações culturais, minicursos entre diversas outras atividades.</p>
+		</div><div class="footer-item">
+			<h4> Endereço </h4>
+			<p> Universidade Estadual do Norte Fluminense Darcy Ribeiro, Av. Alberto Lamego, 2000 - Parque Califórnia, Campos dos Goytacazes - RJ, CEP: 28013-602</p>
+		</div>
+	</footer>
+)
+
